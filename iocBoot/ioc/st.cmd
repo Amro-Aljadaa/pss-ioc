01cdcd7408275e1502145f5889e5746d4950d700
U-SESAME\Mohammad.Abugharbiye <Mohammad.Abugharbiye@User-Abughrbiyeh.SESAME.LOCAL>
2025-01-16 09:39:40 +0300

    added new pvs for the interlock gui of the hmi added new pvs for emergency and intrusion on beamlines optics hutch

--- iocBoot/ioc/st.cmd
+++ iocBoot/ioc/st.cmd
@@ -13,7 +13,14 @@ dbLoadRecords "db/pss-interlocks.db"
 dbLoadRecords "db/pss-ms-status.db"
 dbLoadRecords "db/pss-ms-oh-emergency.db"
 dbLoadRecords "db/pss-ms-shutters.db"
+dbLoadRecords "db/pss-ms-emergency.db"
 dbLoadRecords "db/pss-beats-shutters.db"
+dbLoadRecords "db/pss-beats-emergency.db"
+dbLoadRecords "db/pss-heseb-shutters.db"
+dbLoadRecords "db/pss-xafs-shutters.db"
+dbLoadRecords "db/pss-xafs-emergency.db"
+dbLoadRecords "db/pss-bl-intrusion.db"
+dbLoadRecords "db/radiation-monitors.db"
 dbLoadRecords("$(TOP)/db/iocAdminSoft.db", "IOC=$(IOC_PREFIX)")
 
 EIP_buffer_limit(450)
@@ -21,6 +28,7 @@ drvEtherIP_init()
 drvEtherIP_define_PLC("Booster_PSS_PLC","bo-pss-plc",0)
 drvEtherIP_define_PLC("MS_PSS_PLC","10.2.5.70",0)
 drvEtherIP_define_PLC("BEATS_PSS_PLC","10.2.5.90",0)
+drvEtherIP_define_PLC("XAFS_PSS_PLC","10.2.5.30",0)
 
 # asSetFilename("$(TOP)/common.acf")
 
